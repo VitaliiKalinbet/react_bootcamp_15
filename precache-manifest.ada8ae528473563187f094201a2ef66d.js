bcd5d118ce39817274986f5784faf836e0e2f31b
VitaliiKalinbet <bosssvit@gmail.com>
2019-12-02 13:28:09 +0200

    Updates

---
+++ precache-manifest.ada8ae528473563187f094201a2ef66d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cf1cd76e84205a62c0aee9a75c088e",
+    "url": "/react_bootcamp_15/index.html"
+  },
+  {
+    "revision": "752d65795a2ec7bfa502",
+    "url": "/react_bootcamp_15/static/js/2.17cc5efc.chunk.js"
+  },
+  {
+    "revision": "723b47f748758f3b7a09",
+    "url": "/react_bootcamp_15/static/js/main.6cad8485.chunk.js"
+  },
+  {
+    "revision": "ce0954640049256ca665",
+    "url": "/react_bootcamp_15/static/js/runtime-main.eeca8cbc.js"
+  }
+]);
