c8686be8175a95972a2f9371d8c2fb4acbdd5cda
VitaliiKalinbet <bosssvit@gmail.com>
2019-12-15 23:14:36 +0200

    21 jest testing done

--- src/components/TodoEditor/TodoEditor.js
+++ src/components/TodoEditor/TodoEditor.js
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 
 class TodoEditor extends Component {
-  static propTypes = {
-    onSave: PropTypes.func.isRequired
-  };
-
   state = {
     text: ""
   };
